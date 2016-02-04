fadbcd02cf06e25e140c6d479cff46087dec8834
linroex <linroex@coder.tw>
2016-02-04 21:23:14 +0800

    restore as Post object

--- custom_object.py
+++ custom_object.py
@@ -1,7 +1,7 @@
 from json import dump
 
 class Facebook_Post:
-    def __init__(self, message, created_time, picture):
+    def __init__(self, message, created_time, picture = ''):
         self.message = message
         self.created_time = created_time
         self.picture = picture
--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 from facebook import Facebook
-from custom_object import Facebook_Post
+from custom_object import Facebook_Post as Post
 from config import config
-
-import json
 
 def main():
     fb = Facebook(config['access_token'])
-    # print(posts['paging']['next'])
+    posts = []
 
     for i in range(1,20):
         
         if i == 1:
-            posts = fb.api('get', '/859728857453185/posts')
+            response = fb.api('get', '/859728857453185/posts', {'fields': 'message,full_picture,created_time', 'limit': '100'})
         else:
-            if 'paging' not in posts.keys():
+            if 'paging' not in response.keys():
                 break
             else:
-                posts = fb.next(posts['paging']['next'])
+                response = fb.next(response['paging']['next'])
 
-        for item in posts['data']:
-            # print(item)
-            pass
+        for item in response['data']:
+            if 'message' in item.keys():
+                if 'full_picture' in item.keys():
+                    posts.append(Post(item['message'], item['created_time'], item['full_picture']))
+                else:
+                    posts.append(Post(item['message'], item['created_time']))
+                print(len(posts))
+    
+    print(posts)
         
-        print(i)
 
 if __name__ == '__main__':
     main()
